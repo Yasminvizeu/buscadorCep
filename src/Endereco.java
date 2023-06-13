f1fd90b189f0e3fc30fd2df77f5f38a3f9c9983a
Yasmin Vizeu Gil <yasmin.gil@meta.com.br>
2023-06-13 16:06:31 -0300

    first commit

---
+++ src/Endereco.java
@@ -0,0 +1,7 @@
+public record Endereco(String logradouro, String cep,
+                       String localidade,String complemento, String uf) {
+}
+
+//classe pra representar o endereço com os dados que eu quero, portanto uso o record
+
+
